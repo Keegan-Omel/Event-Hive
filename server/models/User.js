84dea477a8af450e02c218d8d2fb7d7665d13b3c
Cynthia Mory <cynthia.mory.morales@gmail.com>
2023-08-02 05:50:09 -0400

    update model>user.js w/ comments validation>login

--- server/models/User.js
+++ server/models/User.js
@@ -1,46 +1,72 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+// IMPORT MONGOOSE PACKAGE FOR WORKING WITH MONGODB DATABASE
+const mongoose = require("mongoose");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-  events: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Event',
+// IMPORT BCRYPTJS PACKAGE FOR PASSWORD HASHING AND COMPARISON
+const bcryptjs = require("bcryptjs");
+
+// DEFINE THE SCHEMA FOR THE USER MODEL
+const userSchema = new mongoose.Schema(
+  {
+    // USERNAME OF THE USER
+    username: {
+      type: String,
+      required: true, // USERNAME IS REQUIRED
+      unique: true, // USERNAME MUST BE UNIQUE
+      trim: true, // REMOVE EXTRA SPACES FROM USERNAME
     },
-  ],
-});
-
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
+    // EMAIL ADDRESS OF THE USER
+    email: {
+      type: String,
+      required: true, // EMAIL ADDRESS IS REQUIRED
+      unique: true, // EMAIL ADDRESS MUST BE UNIQUE
+      validate: {
+        // VALIDATE THE EMAIL ADDRESS FORMAT USING REGULAR EXPRESSION
+        validator: function (value) {
+          return /^\S+@\S+\.\S+$/.test(value);
+        },
+        //message: "Invalid email address",
+      },
+    },
+    // PASSWORD OF THE USER
+    password: {
+      type: String,
+      required: true, // PASSWORD IS REQUIRED
+      minlength: 8, // PASSWORD MUST HAVE AT LEAST 8 CHARACTERS
+    },
+    // EVENTS ASSOCIATED WITH THE USER (AN ARRAY OF EVENT OBJECT IDS)
+    events: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Event", // REFERENCING THE "Event" MODEL
+      },
+    ],
+  },
+  {
+    timestamps: false, // DISABLE AUTOMATIC TIMESTAMP GENERATION
+    versionKey: false, // DISABLE THE __v FIELD
   }
+);
 
-  next();
+// MIDDLEWARE FUNCTION TO HASH THE PASSWORD BEFORE SAVING THE USER TO DATABASE
+userSchema.pre("save", async function (next) {
+  // CHECK IF THE PASSWORD IS NOT MODIFIED (e.g., in an update operation)
+  if (!this.isModified("password")) {
+    return next(); // IF NOT MODIFIED, SKIP THE HASHING AND PROCEED
+  }
+  const salt = await bcryptjs.genSalt(10); // GENERATE A SALT FOR HASHING
+  this.password = await bcryptjs.hash(this.password, salt); // HASH THE PASSWORD USING THE SALT
+  next(); // CALL THE NEXT FUNCTION TO CONTINUE THE SAVE OPERATION
 });
 
-userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
+// METHOD TO COMPARE THE LOGIN PASSWORD WITH THE USER'S HASHED PASSWORD
+userSchema.methods.checkPassword = async function (loginPw) {
+  return await bcryptjs.compare(loginPw, this.password); // COMPARE THE HASHED PASSWORDS
 };
 
-const User = model('User', userSchema);
+// CREATE THE "User" MODEL USING THE SCHEMA
+const User = mongoose.model("User", userSchema);
 
+// EXPORT THE "User" MODEL
 module.exports = User;
+
 
