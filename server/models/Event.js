4c38a36e5e3517b6f717b17ea115f8db4c1ff89d
Cynthia Mory <cynthia.mory.morales@gmail.com>
2023-08-02 05:47:09 -0400

    revised model > event.js and w/ comments - working

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,62 +1,59 @@
-const { Schema, model, mongoose } = require("mongoose");
-const dateFormat = require("../utils/dateFormat");
+// IMPORT MONGOOSE PACKAGE FOR WORKING WITH MONGODB DATABASE
+const mongoose = require("mongoose");
 
-const eventSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: "You need to leave an event description!",
-      minlength: 1,
-      trim: true,
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    date: {
-      type: Date,
-      required: true,
-      // get: (timestamp) => dateFormat(timestamp),
-    },
-    cost: {
-      type: Number,
-    },
-    seating: {
-      type: Number,  
-    },
-    location: {
-      type: String,
-      required: true,
-    },
-    attendees: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    }],
-
+// DEFINE THE SCHEMA FOR THE EVENT MODEL
+const eventSchema = new mongoose.Schema({
+  // TITLE OF THE EVENT
+  title: {
+    type: String,
+    required: true, // TITLE IS REQUIRED
   },
-  { timestamps: true },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
-);
+  // DESCRIPTION OF THE EVENT
+  description: {
+    type: String,
+    required: true, // DESCRIPTION IS REQUIRED
+    minlength: 1, // DESCRIPTION SHOULD HAVE AT LEAST 1 CHARACTER
+  },
+  // COST OF THE EVENT (OPTIONAL)
+  cost: {
+    type: Number,
+  },
+  // LOCATION OF THE EVENT
+  location: {
+    type: String,
+    required: true, // LOCATION IS REQUIRED
+  },
+  // USER ASSOCIATED WITH THE EVENT
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // REFERENCING THE "User" MODEL
+  },
+  // ATTENDEES FOR THE EVENT (AN ARRAY OF USER OBJECT IDS)
+  attendees: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // REFERENCING THE "User" MODEL
+  }],
+  // DATE OF THE EVENT
+  date: {
+    type: Date,
+    required: true, // DATE IS REQUIRED
+  },
+}, { timestamps: true });
 
+// CREATE THE "Event" MODEL USING THE SCHEMA
 const Event = mongoose.model("Event", eventSchema);
 
-Event.schema.path("date").validate(function (value) {
+// DEFINE VALIDATORS FOR THE SCHEMA
+// VALIDATE THE NUMBER OF ATTENDEES FOR AN EVENT NOT EXCEEDS 50
+Event.schema.path('attendees').validate(function (value) {
+  return value.length <= 50; // SET THE MAXIMUM NUMBER OF ATTENDEES FOR AN EVENT TO 50
+}, 'The number of attendees for an event cannot exceed 50.');
+
+// VALIDATE THAT THE DATE FOR AN EVENT IS IN THE FUTURE
+Event.schema.path('date').validate(function (value) {
   const currentDate = new Date();
-  return value > currentDate;
-}, "The date for an event must be in the future.");
+  return value > currentDate; // SET THE DATE FOR AN EVENT TO BE IN THE FUTURE
+}, 'The date for an event must be in the future.');
 
-
-eventSchema.virtual("seatingFull").get(function () {
-  return this.attendees.length >= this.seating;
-});
-
-
+// EXPORT THE "Event" MODEL
 module.exports = Event;
