0e940aae2525153a3edca58301ce1d07b04a4d46
Cynthia Mory <cynthia.mory.morales@gmail.com>
2023-08-01 20:30:11 -0400

    bcrypt

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,117 +16,24 @@ const typeDefs = gql`
     user: User
     date: String!
     cost: Int
-
     seating: Int
-    
     location: String!
     attendees: [User]
     seatingFull: Boolean
   }
 
   type Query {
-
-    users {
-      _id
-      username
-      email
-      events {
-        _id
-        title
-        description
-        cost
-        date
-        location
-        seatingFull
-        attendees {
-          _id
-          username
-        }
-      }
-    }
-
-    user(_id: $id) {
-      _id
-      username
-      email
-      events {
-        _id
-        title
-      }
-    }
-
-    events {
-      _id
-      title
-      description
-      cost
-      date
-      location
-      seatingFull
-      user {
-        _id
-        username
-      }
-      attendees {
-        _id
-        username
-      }
-    }
-
-    event(_id: $id) {
-      _id
-      title
-      description
-      cost
-      date
-      location
-      seatingFull
-      user {
-        _id
-        username
-      }
-      attendees {
-        _id
-        username
-      }
-    }
-
+    users: [User]!
+    user(id: ID!): User
+    events: [Event]!
+    event(id: ID!): Event
   }
 
   type Mutation {
+    createUser(username: String!, email: String!, password: String!): User # Add the argument definition for createUser mutation
 
-    createUser(username: $username, email: $email, password: $password) {
-      _id
-      username
-      email
-      events {
-        _id
-        title
-      }
-      
-    }
-
-    createEvent(title: $title, description: $description, date: $date, location: $location, cost: $cost, seating: $seating) {
-      _id
-      title
-      description
-      cost
-      date
-      location
-      seatingFull
-      user {
-        _id
-        username
-      }
-      attendees {
-        _id
-        username
-      }
-    }
-
+    createEvent(title: String!, description: String!, date: String!, location: String!, cost: Int!, seating: Int): Event # Add the argument definition for createEvent mutation
   }
 `;
 
 module.exports = typeDefs;
-
-//checkPoint!!!!!NEW!!!!
