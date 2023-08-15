6ea960eef777ac39536735350e5e8cb9385b6d98
MHMD101 <atrach.muhammad@gmail.com>
2023-08-14 20:34:27 -0400

    Updated navBar and Profile components!

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,39 +2,42 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Auth from '../utils/auth';
 import '../assets/css/NavBar.css';
-
 function NavBar() {
   const navigate = useNavigate();
   const [showDropdown, setShowDropdown] = useState(false);
-
   const handleLogout = () => {
     Auth.logout();
     navigate('/');
   };
-
+  // Check if the user is authenticated (you can replace this with your actual authentication logic)
+  const isAuthenticated = Auth.loggedIn();
   return (
     <nav className="navbar">
       <Link to="/">Home</Link>
       <Link to="/profile">Profile</Link>
-
       <div className="dropdown">
         <button className="dropdown-button" onClick={() => setShowDropdown(!showDropdown)}>
-          Sign Up / Sign In
+          {isAuthenticated ? 'Sign Out' : 'Sign Up / Sign In'}
         </button>
         {showDropdown && (
           <div className="dropdown-content">
-            <div>
-              <Link to="/signup">Sign Up</Link>
-            </div>
-            <div>
-              <Link to="/login">Sign In</Link>
-            </div>
-            <button className="signout-button" onClick={handleLogout}>Sign Out</button>
+            {!isAuthenticated && (
+              <>
+                <div>
+                  <Link to="/signup">Sign Up</Link>
+                </div>
+                <div>
+                  <Link to="/login">Sign In</Link>
+                </div>
+              </>
+            )}
+            {isAuthenticated && (
+              <button className="signout-button" onClick={handleLogout}>Sign Out</button>
+            )}
           </div>
         )}
       </div>
     </nav>
   );
 }
-
 export default NavBar;
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_ONE_USER } from '../utils/queries.js'; 
+import Auth from '../utils/auth';
 
 function Profile() {
   // LATER ON THIS SHOULD BE TAKEN FROM THE LOGIN PAGE ONCE THAT IS DONE
-  // This is an example of user1 ID: "64d2e14f11bfea41295d9796" --> used for example purposes only. 
+  // This is an example of user1 ID: "64dab5a419281cb04961176d" --> used for example purposes only. 
   // Again, the correct id will later on be extracted from the login data, etc.
-  const _id = '64dab5a419281cb04961176d';
+  console.log(Auth.getProfile())
+  const _id = Auth.getProfile() 
 
   // Use the GET_ONE_USER query with variables to fetch the user's data
   const { loading, error, data } = useQuery(GET_ONE_USER, {
